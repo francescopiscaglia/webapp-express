4abb95075b033cc070c7cdc2bd4533e26e4e9ba3
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-12-10 12:16:23 +0100

    add index route

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const express = require("express");
 const app = express();
+
+const PORT = process.env.PORT || 3009;
+const HOST = process.env.HOST || "http://localhost";
+
+
+// start the server
+app.listen(PORT, () => {
+    console.log(`server is running at ${HOST}:${PORT}`)
+});
+
+// index
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
